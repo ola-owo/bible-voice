ced82e91b4a63924c0673330a63b7cfad1112260
Ola Owoputi <technoto@gmail.com>
2016-12-27 00:42:00 -0500

    added api.ai integration

--- app.js
+++ app.js
@@ -8,75 +8,107 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var db = new sqlite3.cached.Database('esv.sqlite3', sqlite3.OPEN_READONLY);
 
 function getChapVerse(chap,verse){
-	// Convert chapter/verse to REAL
-	var chap_verse = chap + ".";
-	var v = verse;
-	var spaces = 3 - verse.length;
-	for(var i=0; i < spaces; i++){
-		v = '0' + v;
-	}
-	chap_verse += v;
-	return parseFloat(chap_verse);
+  // Convert chapter/verse to REAL
+  var chap_verse = chap + ".";
+  var v = verse;
+  var spaces = 3 - verse.length;
+  for(var i=0; i < spaces; i++){
+    v = '0' + v;
+  }
+  chap_verse += v;
+  return parseFloat(chap_verse);
 }
 
 
 app.get('/', function(req,res){
-	res.write('<html><body>');
-	res.write('<form method="post" action="/lookup">');
-	res.write('Book <input type="text" name="book"><br>');
-	res.write('Chapter <input type="text" name="chapter"><br>');
-	res.write('Verse <input type="text" name="verse"><br>');
-	res.write('<button type="submit">Go</button>');
-	res.write('</form>');
-	res.write('</body></html>');
-	res.end();
+  res.write('<html><body>');
+  res.write('<form method="post" action="/">');
+  res.write('Book <input type="text" name="book"><br>');
+  res.write('Chapter <input type="text" name="chapter"><br>');
+  res.write('Verse <input type="text" name="verse"><br>');
+  res.write('<button type="submit">Go</button>');
+  res.write('</form>');
+  res.write('</body></html>');
+  res.end();
+});
+
+app.post('/', function(req,res){
+  var book, chapter, verse;
+  book = req.body.book,
+  chapter = req.body.chapter,
+  verse = req.body.verse;
+
+  // Search database & get bible verse
+  var bibleText = '';
+  var chapverse = getChapVerse(chapter, verse);
+  console.log("chapverse: " + chapverse);
+  var stmt = db.prepare('SELECT * FROM verses WHERE book = $book AND verse = $chapverse;', {
+    $book: book,
+    $chapverse: chapverse
+  });
+  stmt.get([], function(err,row){
+    bibleText = row.unformatted;
+
+    console.log('BibleText: ' + bibleText);
+    res.end(bibleText);
+  });
 });
 
 app.get('/lookup', function(req,res){
 
-	// **SAMPLE BIBLE VERSE**
-	book = 'Rom';
-	chapter = '5';
-	verse = '8';	
+  // **SAMPLE BIBLE VERSE**
+  book = 'Rom';
+  chapter = '5';
+  verse = '8';  
 
-	res.write("<h1>" + book + " " + chapter + ":" + verse + "</h1><br>")
-	// Search database & get bible verse
-	var bibleText = '';
-	var chapverse = getChapVerse(chapter, verse);
-	console.log("chapverse: " + chapverse);
-	var stmt = db.prepare('SELECT * FROM verses WHERE book = $book AND verse = $chapverse;', {
-		$book: book,
-		$chapverse: chapverse
-	});
-	stmt.get([], function(err,row){
-		bibleText = row.unformatted;
-		res.end(bibleText);
-	});
+  res.write("<h1>" + book + " " + chapter + ":" + verse + "</h1><br>")
+  // Search database & get bible verse
+  var bibleText = '';
+  var chapverse = getChapVerse(chapter, verse);
+  console.log("chapverse: " + chapverse);
+  var stmt = db.prepare('SELECT * FROM verses WHERE book = $book AND verse = $chapverse;', {
+    $book: book,
+    $chapverse: chapverse
+  });
+  stmt.get([], function(err,row){
+    bibleText = row.unformatted;
+    res.end(bibleText);
+  });
 });
 
 app.post('/lookup', function(req,res){
-	var book, chapter, verse;
-	book = req.body.book,
-	chapter = req.body.chapter,
-	verse = req.body.verse;
+  // For the API.ai program
+  
+  res.type('json');  
 
-	// Search database & get bible verse
-	var bibleText = '';
-	var chapverse = getChapVerse(chapter, verse);
-	console.log("chapverse: " + chapverse);
-	var stmt = db.prepare('SELECT * FROM verses WHERE book = $book AND verse = $chapverse;', {
-		$book: book,
-		$chapverse: chapverse
-	});
-	stmt.get([], function(err,row){
-		bibleText = row.unformatted;
+  var book = req.body.result.parameters.book,
+      chapter = req.body.result.parameters.chapter,
+      verse = req.body.result.parameters.verse;
 
-		console.log('BibleText: ' + bibleText);
-		res.end(bibleText);
-	});
+  var output = {};
+
+  // Search database & get bible verse
+  var bibleText = '';
+  var chapverse = getChapVerse(chapter, verse);
+  console.log("chapverse: " + chapverse);
+  var stmt = db.prepare('SELECT * FROM verses WHERE book = $book AND verse = $chapverse;', {
+    $book: book,
+    $chapverse: chapverse
+  });
+  stmt.get([], function(err,row){
+    bibleText = row.unformatted;
+    console.log('BibleText: ' + bibleText);
+
+    output.speech = bibleText;
+    output.displayText = book + ' ' + chapter + ':' + verse + '\n' + bibleText;
+    output.data = {};
+    output.contextOut = [{"book": book, "chapter": chapter, "verse": verse}];
+    output.source = 'English Standard Version';
+    res.end(JSON.stringify(output));
+  });
 });
 
 var port = process.env.PORT || 8000;
 app.listen(port, function(){
-	console.log('App.js listening on port ' + port);
+  console.log('App.js listening on port ' + port);
 });
